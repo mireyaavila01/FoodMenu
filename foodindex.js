9b99c4d7f5c27ce4375de17ce1dab2578dd6129d
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-10 02:05:17 -0400

    created html

---
+++ foodindex.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let menu = {
+    drinks : [
+    "Water", "Tea", "Sweet Tea",
+    "Coke", "Dr. Pepper", "Sprite"
+    ],
+    entrees : [
+    "Hamburger w/ Fries",
+    "Grilled Cheese w/ Tater Tots",
+    "Grilled Chicken w/ Veggies",
+    "Chicken Fried Steak w/ Mashed Potatoes",
+    "Fried Shrimp w/ Coleslaw",
+    "Veggie Plate"
+    ],
+    desserts: [
+    "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+    ]
+    };
+
+    
